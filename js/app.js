6287f78473e92fc48c9d0a861155fc224101bf2d
Amit Patel <amit@mramitpatel.com>
2014-11-20 21:03:29 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function(){
+
+var randomNum;
+var guessCount;
+
+
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+  	});
+
+	/*--- New game ---*/
+  	$(".new").click(function(){
+		newGame();
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".info").fadeOut(1000);
+  		$(".overlay").fadeOut(1000);
+  	});
+
+
+	var newGame = function(){
+		randomNum = Math.floor((Math.random() * 100) + 1);
+		guessCount = 1;
+		console.log(randomNum);
+		$("#feedback").text("Make a Guess");
+		$("#guessList").text(" ");
+		$('#userGuess').val(" ");
+		$('#count').text("1");
+		$(".overlay").hide();
+	};
+ 		newGame();
+	
+	$('#guessButton').click(function(){
+		userGuess = $('#userGuess').val();
+		if (userGuess >= 1 && userGuess <= 100){
+			guessCount++;
+			$('#count').text(guessCount);
+			console.log(userGuess);
+			listGuess(userGuess);
+			compare(userGuess);
+			return false;
+
+		} else {
+			$("#feedback").text("Please make a guess between 1 and 100");
+			return false;
+		}
+	});
+
+	var listGuess = function(userGuess){
+		$("#guessList").append("<li>" + userGuess + "</li>");
+	};
+
+
+	var compare = function(userGuess){
+		var difference = Math.abs(userGuess - randomNum);
+		if (userGuess == randomNum){
+			winner();
+
+		} else if
+			(difference <= 10){
+			$("#feedback").text("very hot");	
+		}
+		else if (difference > 11 && difference < 20) {
+			$("#feedback").text("hot");
+		}
+		else if (difference > 21 && difference < 30) {
+			$("#feedback").text("warm");
+		}
+		else if (difference > 31 && difference < 50) {
+			$("#feedback").text("cold");
+		}
+		else {
+			$("#feedback").text("very cold");
+		}
+	}
+
+	var clearfield = function(){
+		$('#userGuess').val(" ");
+	}
+	
+
+	var winner = function(){
+		$("#feedback").text("WINNER!");
+		$("#info").hide();
+		$(".overlay").fadeIn(1000);
+    	$(".winner").fadeIn(1000);
+  	}
+
+});
+
+
